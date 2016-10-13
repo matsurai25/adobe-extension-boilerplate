99bd439c828d26b603b35b03130efbe66d9d02ca
matsurai25 <sota_matsumoto@waku-2.com>
2016-10-14 00:45:17 +0900

    証明書作成、zxp作成タスクの追加

---
+++ src/config.js
@@ -0,0 +1,5 @@
+var config = {
+  id:'info.matsurai25',
+  name:'sample_app',
+  version:'0.0.0',
+};
---
+++ src/js/application.js
@@ -0,0 +1,3 @@
+window.$ = require('jquery'); // 色々
+window._ = require('underscore'); // js処理拡張
+window.moment = require('moment'); // 日時を使いやすく
